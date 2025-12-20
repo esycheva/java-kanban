51895c9f19b46aea0cbfdf7d985674584b89779e
Елена Истомина <69IstominaEA@region.cbr.ru>
2025-12-20 18:45:54 +0300

    fix codestyle

--- src/ru/yandex/practicum/Timetable.java
+++ src/ru/yandex/practicum/Timetable.java
@@ -8,41 +8,39 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Timetable {
-    
     private Map<DayOfWeek, Map<TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();
     private CounterComparator counterComparator = new CounterComparator();
-    
+
     private Comparator<TimeOfDay> comparator = new Comparator<>() {
     	@Override
     	public int compare(TimeOfDay t1, TimeOfDay t2) {
-    		if(t1.getHours() == t2.getHours() && t1.getMinutes() == t2.getMinutes()) {
+    		if (t1.getHours() == t2.getHours() && t1.getMinutes() == t2.getMinutes()) {
     			return 0;
     		}
-    		
-    		if(t1.getHours() > t2.getHours()) {
+
+    		if (t1.getHours() > t2.getHours()) {
     			return 1;
     		}
-    		
-    		if(t1.getHours() == t2.getHours() && t1.getMinutes() > t2.getMinutes()) {
+
+    		if (t1.getHours() == t2.getHours() && t1.getMinutes() > t2.getMinutes()) {
     			return 1;
     		}
     		return -1;
     	}
     };
-    
+
     public void addNewTrainingSession(TrainingSession trainingSession) {
         //сохраняем занятие в расписании
     	Map<TimeOfDay, ArrayList<TrainingSession>> day;
-    	
-    	if(timetable.containsKey(trainingSession.getDayOfWeek())) {
+
+    	if (timetable.containsKey(trainingSession.getDayOfWeek())) {
     		day = timetable.get(trainingSession.getDayOfWeek());
     	} else {
     		day = new TreeMap<>(comparator);
     	}
-    	    	
+    	
     	ArrayList<TrainingSession> sessionsInTime;
-    	
-    	
+
     	if (day.containsKey(trainingSession.getTimeOfDay())) {
     		sessionsInTime = day.get(trainingSession.getTimeOfDay());
     	} else {
@@ -50,7 +48,7 @@ public class Timetable {
     	}
     	sessionsInTime.add(trainingSession);
     	System.out.println(sessionsInTime);
-    	
+
     	day.put(trainingSession.getTimeOfDay(), sessionsInTime);
     	timetable.put(trainingSession.getDayOfWeek(), day);
     }
@@ -58,10 +56,10 @@ public class Timetable {
     public Map<TimeOfDay, ArrayList<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
     	return timetable.get(dayOfWeek);
     }
-    
+
     public ArrayList<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
     	ArrayList<TrainingSession> result;
-    	
+
     	if(timetable.containsKey(dayOfWeek)) {
         	Map<TimeOfDay, ArrayList<TrainingSession>> day = timetable.get(dayOfWeek);
         	
@@ -75,28 +73,28 @@ public class Timetable {
         }
     	return result;
     }
-    
+
     public ArrayList<CounterOfTrainings> getCountByCoaches() {
     	ArrayList<CounterOfTrainings> counters = new ArrayList<>();
-    	
+
     	for (Map.Entry<DayOfWeek, Map<TimeOfDay, ArrayList<TrainingSession>>> entry : timetable.entrySet()) {
     		Map<TimeOfDay, ArrayList<TrainingSession>> day = entry.getValue();
-    		
+
     		for(Map.Entry<TimeOfDay, ArrayList<TrainingSession>> e : day.entrySet()) {
     			ArrayList<TrainingSession> sessions = e.getValue();
-    			
+
     			for(TrainingSession session : sessions) {
     				Coach coach = session.getCoach();
-    				
+
     				CounterOfTrainings matchCounter = null;
-    				
+
     				for(CounterOfTrainings c : counters) {
     					if(c.getFio().equals(coach.getFio())) {
     						c.incrementCounter();
     						matchCounter = c;
     					}
     				}
-    				
+
     				if (matchCounter == null) {
     					CounterOfTrainings counter = new CounterOfTrainings(coach.getFio());
     					counter.incrementCounter();
