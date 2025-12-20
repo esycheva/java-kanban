42484e3a4e04ed37439b81791dadeafd5efcccb5
Елена Истомина <69IstominaEA@region.cbr.ru>
2025-12-20 18:57:59 +0300

    fix codestyle

--- src/ru/yandex/practicum/CounterComparator.java
+++ src/ru/yandex/practicum/CounterComparator.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum;
 
 import java.util.Comparator;
 
-public class CounterComparator implements Comparator<CounterOfTrainings>{
+public class CounterComparator implements Comparator<CounterOfTrainings> {
 	@Override
 	public int compare(CounterOfTrainings c1, CounterOfTrainings c2) {
 		if (c1.getCounter() == c2.getCounter()) {
